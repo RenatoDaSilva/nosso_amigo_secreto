baf8ca2061a01059491f91801498efc530bf90f0
RenatoDaSilva <renato@asseinfo.com.br>
2018-02-04 12:11:39 -0200

    set models campaign and member

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,16 @@
 class Campaign < ApplicationRecord
   belongs_to :user
+  has_many :members, dependent: :destroy
+  before_create :set_member
+  before_create :set_status
+  enum status: %i[pending finished]
+  validates :title, :description, :user, :status, presence: true
+
+  def set_status
+    self.status = :pending
+  end
+
+  def set_member
+    members << Member.create(name: user.name, email: user.email)
+  end
 end
